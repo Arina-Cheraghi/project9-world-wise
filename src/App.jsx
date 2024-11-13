bd4ad68ffa6196d15700711a65168781e7763b01
arina-cheraghi <arina.chr81@gmail.com>
2024-11-13 21:30:39 +0330

    add login section

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,37 @@ import City from "./components/City";
 import Form from "./components/Form";
 import Map from "./components/Map";
 import { CitiesProvider } from "./contexts/CitiesContext";
-
+import { AuthProvider } from "./contexts/fakeAuthContext";
+import ProtectedRoute from "./pages/protectedRoute";
 function App() {
   return (
     <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-            <Route path="map" element={<Map />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+              <Route path="map" element={<Map />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </CitiesProvider>
   );
 }
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
+import { FaUserAstronaut } from "react-icons/fa6";
+
 
 function PageNav() {
   return (
@@ -14,7 +16,7 @@ function PageNav() {
           <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
+          <NavLink to="/login" className={styles.ctaLink}><FaUserAstronaut /></NavLink>
         </li>
       </ul>
     </nav>
